e52234280456a0b7eaa305fc3c853391637b951f
maxeozakh <maxeozakh@gmail.com>
2022-05-20 11:47:13 +0400

    db schema drafts

--- wishl/__init__.py
+++ wishl/__init__.py
@@ -26,6 +26,6 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello, someone'
 
     return app
