2370ea1f35344da15b2758d415141611d453c649
Maxim Zakharov <maxeozakh@gmail.com>
2022-10-27 22:33:02 +0400

    delete unused stuff

--- wishl/__init__.py
+++ wishl/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import (Flask)
 from flask_cors import CORS
 
 def create_app(test_config=None):
@@ -33,9 +33,6 @@ def create_app(test_config=None):
     
     from . import db
     db.init_app(app)
-
-    from . import auth
-    app.register_blueprint(auth.bp)
 
     from . import wishes
     app.register_blueprint(wishes.bp)
